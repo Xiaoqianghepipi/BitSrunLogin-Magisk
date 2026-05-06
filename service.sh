9038f576a88e2035c7e03306e58ba6439f73363b
xiaoqianghepipi <wqwqwqwqds@outlook.com>
2026-05-06 20:03:49 +0800

    去掉没用的action（也就是那个执行按钮）

--- service.sh
+++ service.sh
@@ -1,5 +1,13 @@
 #!/data/adb/magisk/busybox sh
 MODDIR=${0%/*}
+MODULE_PROP="${MODDIR}/module.prop"
+
+# 更新module.prop文件中的description
+update_module_description() {
+  local status_message=$1
+  sed -i "/^description=/c\description=[状态]${status_message}" ${MODULE_PROP}
+}
+
 # MODDIR="$(dirname $(readlink -f "$0"))"
 chmod 755 ${MODDIR}/*
 
@@ -11,8 +19,18 @@ done
 # 防止系统挂起
 echo "PowerManagerService.noSuspend" > /sys/power/wake_lock
 
-# 修改模块描述
-sed -i 's/$(description=)$[^"]*/\1[状态]关闭中/' "$MODDIR/module.prop"
+# 启动守护脚本前先同步一次状态
+if [ -f "${MODDIR}/disable" ]; then
+  update_module_description "已关闭"
+elif pgrep -f 'bitsrun' >/dev/null; then
+  if [ -f "${MODDIR}/config/command_args" ]; then
+    update_module_description "主程序已开启(启动参数模式)"
+  else
+    update_module_description "主程序已开启(配置文件模式)"
+  fi
+else
+  update_module_description "关闭中"
+fi
 
 # 等待 3 秒
 sleep 3s
