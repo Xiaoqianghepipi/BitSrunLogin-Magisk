c4ec211b38a693f570cb4d242464f8609cd27476
xiaoqianghepipi <wqwqwqwqds@outlook.com>
2026-05-06 20:21:09 +0800

    模块不再同步bitsrun的版本

--- customize.sh
+++ customize.sh
@@ -1,6 +1,15 @@
 ui_print '安装完成'
 ui_print '当前架构为' + $ARCH
 ui_print '当前系统版本为' + $API
+
+CORE_VERSION_FILE="${MODPATH}/CORE_VERSION"
+if [ -f "${CORE_VERSION_FILE}" ]; then
+	CORE_VERSION_VALUE="$(cat "${CORE_VERSION_FILE}")"
+	ui_print "bitsrungo 最新release版本: ${CORE_VERSION_VALUE}"
+else
+	ui_print 'bitsrungo 最新release版本: unknown'
+fi
+
 ui_print '安装目录为:  /data/adb/modules/bitsrun_magisk'
 ui_print '配置文件位置:  /data/adb/modules/bitsrun_magisk/config/config.yaml'
 ui_print '如果需要自定义启动参数，可将 /data/adb/modules/bitsrun_magisk/config/command_args_sample 重命名为 command_args，并修改其中内容，使用自定义启动参数时会忽略配置文件'
